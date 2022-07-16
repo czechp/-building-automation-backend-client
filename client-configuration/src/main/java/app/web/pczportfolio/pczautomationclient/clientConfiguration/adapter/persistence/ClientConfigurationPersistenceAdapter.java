0e03bd3b17cec3e137352ec13819c05b17a56083
czechp <webcoderc@gmail.com>
2022-07-16 23:14:45 +0200

    Port to find existing configuration

--- client-configuration/src/main/java/app/web/pczportfolio/pczautomationclient/clientConfiguration/adapter/persistence/ClientConfigurationPersistenceAdapter.java
+++ client-configuration/src/main/java/app/web/pczportfolio/pczautomationclient/clientConfiguration/adapter/persistence/ClientConfigurationPersistenceAdapter.java
@@ -1,10 +1,13 @@
 package app.web.pczportfolio.pczautomationclient.clientConfiguration.adapter.persistence;
 
 import app.web.pczportfolio.pczautomationclient.clientConfiguration.application.port.ClientConfigurationPortConfigurationExists;
+import app.web.pczportfolio.pczautomationclient.clientConfiguration.application.port.ClientConfigurationPortFindExisting;
 import app.web.pczportfolio.pczautomationclient.clientConfiguration.application.port.ClientConfigurationPortSave;
 import app.web.pczportfolio.pczautomationclient.clientConfiguration.domain.ClientConfiguration;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -17,5 +20,13 @@ class ClientConfigurationPersistenceAdapter implements
     public ClientConfiguration save(ClientConfiguration clientConfiguration) {
         final var entityToSave = ClientConfigurationEntity.toEntity(clientConfiguration);
         return ClientConfigurationEntity.toDomain(jpaRepository.save(entityToSave));
+    }
+
+    @Override
+    public Optional<ClientConfiguration> findExistingConfiguration() {
+        return jpaRepository.findAll()
+                .stream()
+                .map(ClientConfigurationEntity::toDomain)
+                .findAny();
     }
 }
