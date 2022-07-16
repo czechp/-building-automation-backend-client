88cd29dd72359583dab19ea13d026d958903b576
czechp <webcoderc@gmail.com>
2022-07-16 17:01:38 +0200

    Add files after reload gitignore

---
+++ core/src/test/java/app/web/pczportfolio/pczautomationclient/pczbuildingautomationclient/PczBuildingAutomationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.pczportfolio.pczautomationclient.pczbuildingautomationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PczBuildingAutomationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
