8c0a6bffdde12d786f50bdd69465d7941e6eb1bb
czechp <webcoderc@gmail.com>
2022-07-16 21:40:33 +0200

    Error in JpaResitory (point to domain instead entity)

--- client-configuration/src/main/java/app/web/pczportfolio/pczautomationclient/adapter/persistence/ClientConfigurationEntityJpaRepository.java
+++ client-configuration/src/main/java/app/web/pczportfolio/pczautomationclient/adapter/persistence/ClientConfigurationEntityJpaRepository.java
@@ -1,10 +1,9 @@
 package app.web.pczportfolio.pczautomationclient.adapter.persistence;
 
-import app.web.pczportfolio.pczautomationclient.domain.ClientConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface ClientConfigurationJpaRepository extends JpaRepository<ClientConfiguration, Long> {
-    
+interface ClientConfigurationEntityJpaRepository extends JpaRepository<ClientConfigurationEntity, Long> {
+
 }
