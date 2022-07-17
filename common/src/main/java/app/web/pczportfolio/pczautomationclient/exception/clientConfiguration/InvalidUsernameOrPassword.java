fccc555f0bbcf053c7f8f3ab11522703530b7462
czechp <webcoderc@gmail.com>
2022-07-17 13:12:45 +0200

    Exception to notify about wrong username or password

---
+++ common/src/main/java/app/web/pczportfolio/pczautomationclient/exception/clientConfiguration/InvalidUsernameOrPassword.java
@@ -0,0 +1,7 @@
+package app.web.pczportfolio.pczautomationclient.exception.clientConfiguration;
+
+public class InvalidUsernameOrPassword extends RuntimeException {
+    public InvalidUsernameOrPassword(String message) {
+        super(message);
+    }
+}
